cfca72ab0942efca158eef88c524e22c6373436a
ClevelandSchemer <samjsmith1996@gmail.com>
2024-09-30 21:28:02 -0400

    Test

--- onnx.js
+++ onnx.js
@@ -58,6 +58,15 @@ async function guess() {
 let drawing = false;
 let started = false;
 
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
 loadingModelPromise.then(() => {  
   canvas.addEventListener('mousedown', () => drawing = true);
   canvas.addEventListener('mouseup', () => {
@@ -65,12 +74,28 @@ loadingModelPromise.then(() => {
       started = false;
   });
   canvas.addEventListener('mousemove', draw);
-  canvas.addEventListener('touchstart', () => drawing = true);
-  canvas.addEventListener('touchend', () => {
-      drawing = false
-      started = false;
+  // Set up touch events for mobile, etc
+  canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+  clientX: touch.clientX,
+  clientY: touch.clientY
   });
-  canvas.addEventListener('touchmove', draw);
+  canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+  clientX: touch.clientX,
+  clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+  }, false);
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillText("Draw a number here!", canvas.width / 2, canvas.height / 2);

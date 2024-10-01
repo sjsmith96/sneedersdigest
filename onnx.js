6e3105b316911fa33ec3ef27b680fde8fd9e5874
ClevelandSchemer <samjsmith1996@gmail.com>
2024-09-30 21:24:48 -0400

    Test

--- onnx.js
+++ onnx.js
@@ -113,3 +113,21 @@ document.getElementById('clear-btn').addEventListener('click', () => {
     started = false;
     isShowingStartText = false;
 });
+
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
